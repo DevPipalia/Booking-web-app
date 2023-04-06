5d5852df4d028123a35e094d3530ec16a0185808
Dev Pipalia <devpipalia003@gmail.com>
2023-04-07 01:58:45 +0530

    initializd feautured component

---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,13 @@
+import "./featured.css";
+import React from "react";
+
+function Featured(){
+    return(
+        <>
+            featured
+        </>
+    )
+}
+
+
+export default Featured;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavigationBar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
+import Featured from "../../components/Featured/Featured";
 
 function HomePage(){
     return(
         <div>
             <NavigationBar/>
             <Header/>
+            <Featured/>
             <p>Home</p>
         </div>
     )
