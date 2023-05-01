ccdef19694f92034d62d82cc65845dff8f512bb0
Dev Pipalia <devpipalia003@gmail.com>
2023-05-02 00:48:54 +0530

    propertylist completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavigationBar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import Featured from "../../components/Featured/Featured";
+import PropertyList from "../../components/propertyList/Propertylist";
 
 function HomePage(){
     return(
@@ -9,6 +10,7 @@ function HomePage(){
             <NavigationBar/>
             <Header/>
             <Featured/>
+            <PropertyList/>
             <p>Home</p>
         </div>
     )
