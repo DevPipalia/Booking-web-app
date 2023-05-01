d1862ce5525a86637670fea86ed44f0003c3726f
Dev Pipalia <devpipalia003@gmail.com>
2023-05-02 00:41:37 +0530

    featured completed

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,35 +1,44 @@
 import "./featured.css";
-import React from "react";
 
-function Featured(){
-    return(
-        <>
-            <div className="featured">
-                <div className="featured-item">
-                    <img src="https://www.ourescapeclause.com/wp-content/uploads/2019/05/CinqueTerre-33.jpg" className="featured-img" />
-                </div>
-                <div className="featured-title f1">
-                    <h1>Italy</h1>
-                    <h2>200 properties</h2>
-                </div>
-                <div className="featured-item">
-                    <img src="https://www.visa.com.au/dam/VCOM/regional/ap/images/travel-with-visa/paris/marquee-travel-paris-800x450.jpg" className="featured-img"/>
-                </div>
-                <div className="featured-title f2">
-                    <h1>Paris</h1>
-                    <h2>100 properties</h2>
-                </div>
-                <div className="featured-item">
-                    <img src="https://travellersworldwide.com/wp-content/uploads/2022/12/Shutterstock_1094901527.jpg" className="featured-img"/>
-                </div>
-                <div className="featured-title f3">
-                    <h1>Sydney</h1>
-                    <h2>123 properties</h2>
-                </div>
-            </div>
-        </>
-    )
-}
-
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Reno</h1>
+          <h2>533 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Austin</h1>
+          <h2>532 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Featured;
