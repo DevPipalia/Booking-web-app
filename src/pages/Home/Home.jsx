37048a561a7b9f42ec067b3e8a6d691db052f91e
Dev Pipalia <devpipalia003@gmail.com>
2023-05-02 00:55:03 +0530

    maillist updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import NavigationBar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import Featured from "../../components/Featured/Featured";
 import PropertyList from "../../components/propertyList/Propertylist";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 function HomePage(){
     return(
@@ -11,6 +12,7 @@ function HomePage(){
             <Header/>
             <Featured/>
             <PropertyList/>
+            <FeaturedProperties/>
             <p>Home</p>
         </div>
     )
